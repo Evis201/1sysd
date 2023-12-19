b766b32d3e5f8d0b729954d85dcf9552c48e8712
Noe <noe@debian.Debian>
2023-12-19 07:04:57 -0600

    Ask number enter 1 and 10

---
+++ TP1/Ask.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+    int nombreAleatoire = rand() % 10 + 1;
+    int tentative, nombreEssais = 0;
+    printf("Devinez le nombre entre 1 et 10\n");
+
+    do {
+        printf("Votre proposition : ");
+        scanf("%d", &tentative);
+
+        nombreEssais++;
+
+        if (tentative < nombreAleatoire) {
+            printf("Plus grand !\n");
+        } else if (tentative > nombreAleatoire) {
+            printf("Plus petit !\n");
+        } else {
+            printf("Bravo ! Vous avez trouvé le nombre en %d essais.\n", nombreEssais);
+        }
+
+    } while (tentative != nombreAleatoire);
+
+    return 0;
+}
+
