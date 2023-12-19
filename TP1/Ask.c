4ec59284ec66e8028c04fc2f53ddc8baa1ef8032
Noe <noe@debian.Debian>
2023-12-19 07:14:47 -0600

    Fix commun bugs

--- TP1/Ask.c
+++ TP1/Ask.c
@@ -6,20 +6,20 @@ int main() {
     srand(time(NULL));
     int nombreAleatoire = rand() % 10 + 1;
     int tentative, nombreEssais = 0;
+    
     printf("Devinez le nombre entre 1 et 10\n");
 
     do {
         printf("Votre proposition : ");
         scanf("%d", &tentative);
+        
         nombreEssais++;
 
         if (tentative < nombreAleatoire) {
             printf("Plus grand !\n");
-        } 
-        else if (tentative > nombreAleatoire) {
+        } else if (tentative > nombreAleatoire) {
             printf("Plus petit !\n");
-        } 
-        else {
+        } else {
             printf("Bravo ! Vous avez trouvé le nombre en %d essais.\n", nombreEssais);
         }
 
