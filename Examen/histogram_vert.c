f84f1fbd943cb19fa6f47074fbf7d558a1dba4b6
Evis201 <83975305+Evis201@users.noreply.github.com>
2025-01-07 10:47:34 +0100

    feat: Add vertical histogram display function

---
+++ Examen/histogram_vert.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void display_histogram_vert(int tab[], int size) {
+    int max = 0;
+    for (int i = 0; i < size; i++) {
+        if (tab[i] > max) {
+            max = tab[i];
+        }
+    }
+
+    for (int i = max; i > 0; i--) {
+        printf("%2d|", i);
+        for (int j = 0; j < size; j++) {
+            if (tab[j] >= i) {
+                printf(" * ");
+            } else {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+
+    printf(" 0|");
+    for (int i = 0; i < size; i++) {
+        printf(" %d ", tab[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int tab[] = {4, 9, 8, 2, 0, 1, 10, 5, 8};
+    int size = sizeof(tab) / sizeof(tab[0]);
+
+    display_histogram_vert(tab, size);
+
+    // return 0;
+}
